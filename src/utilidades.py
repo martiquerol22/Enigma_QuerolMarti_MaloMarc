617c31032a38ec1099f67df4619a25d023ae5bb5
martiquerol22 <marti.querol@estudiant.enti.cat>
2025-12-07 20:41:41 +0100

    creo el archivo Rotor1

--- src/utilidades.py
+++ src/utilidades.py
@@ -2,7 +2,8 @@ def validar_rotores(rotor):
     try:
         with open(rotor,"r") as file:
             data = file.readlines()
+            print(data)
     except FileNotFoundError:
         print(f"ERROR: No se ha encontrado el archivo {rotor}.")
 
-        
+validar_rotores("Rotor1.txt")
