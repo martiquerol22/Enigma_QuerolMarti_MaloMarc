c492412c40b8fc4059a2905cb26b9b3f5e0ee0aa
martiquerol22 <marti.querol@estudiant.enti.cat>
2025-12-10 21:53:24 +0100

    Update main.py
    integro la funcio editar rotor al main

--- src/main.py
+++ src/main.py
@@ -20,7 +20,19 @@ Que vols fer?
             descifrado()
 
         elif opcion == 3:
-            editar_rotor()
+            entrada = input("Quin rotor vols editar (1-3)? ")
+
+            if entrada not in ["1", "2", "3"]:
+                print("[ERROR] Has de triar un número entre 1 i 3.")
+                input("Prem ENTER per tornar al menu...")
+                os.system('cls') # limpia la consola
+                continue # inicia el bucle de nou
+
+            ruta = f"data/Rotor{entrada}.txt"
+            nuevo_wire = input("Escriu la nova permutacio (26 lletres): ").upper()
+            
+            gestor_archivos.editar_rotor(ruta, nuevo_wire)
+            input("\nPrem ENTER per continuar...")
 
         elif opcion == 4:
             print("\nSortint del programa...")
