2cda8301a6e3f13c93e09ca36c901d5f9932ddd6
martiquerol22 <marti.querol@estudiant.enti.cat>
2025-12-10 19:52:36 +0100

    Update gestor_archivos.py
    he fet els logs per aquestes fucions

--- src/gestor_archivos.py
+++ src/gestor_archivos.py
@@ -1,5 +1,6 @@
 def cargar_rotores(rotor):
     try:
+        print(f"[INFO] Llegint {rotor}...")
         with open(rotor,"r") as file:
             data = file.readlines()
             wire = data[0].strip() # elimina el \n
@@ -11,24 +12,29 @@ def cargar_rotores(rotor):
                 notch = "Z"
 
     except FileNotFoundError:
-        print(f"ERROR: No se ha encontrado el archivo {rotor}.")
+        print(f"[ERROR]: No s'ha trobat el arxiu {rotor}.")
         return
 
+    print(f"[OK] Arxiu {rotor} llegit correctament!\n")
     return wire, notch
 
-# wire = cargar_rotores("src/Rotor1.txt")[0]
-
+wire = cargar_rotores("Rotor1.txt")[0]
+rotor = "Rotor1.txt"
 def validar_rotores(wire):
+    print(f"[INFO] Validant {rotor}...")
     if len(wire) != 26:
-        print(f"ERROR: El cableado ha de ser de 26 letras y tiene {len(wire)}.")
+        print(f"[ERROR] {rotor}: permutació incorrecta — calen 26 lletres uniques A-Z.")
         return False # si no se cumple sale de la funcion y devuelve False
 
     """Mira si hay caracteres repetidos"""
     caracteres_vistos = set()
     for char in wire:
         if char in caracteres_vistos:
-            print("ERROR: Hay letras repetidas en el cableado.")
+            print(f"[ERROR] {rotor}: permutació incorrecta — calen 26 lletres uniques A-Z")
             return False # si no se cumple sale de la funcion y devuelve False
         caracteres_vistos.add(char)
-
+    print(f"[OK] {rotor} validat!\n")
     return True # si se cumplen todas las condiciones devuelve True
+
+#cargar_rotores(rotor)
+validar_rotores(wire)
