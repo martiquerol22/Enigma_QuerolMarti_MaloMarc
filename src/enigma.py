ae438edff4dee50d50d1fa19c38aea0a9a342553
marma990 <marc.malo@estudiant.enti.cat>
2025-12-14 18:17:06 +0100

    Encriptación

---
+++ src/enigma.py
@@ -0,0 +1,17 @@
+ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encriptar(letra, wire_1, wire_2, wire_3):
+    if letra not in ALFABETO: 
+        return letra #devuelve el caracter que no esta dentro del alfabeto
+    indice = ALFABETO.index(letra) #busca la posicion de cada letra del mensaje en el alfabeto
+    letra_r1 = wire_1[indice] #la letra del primer rotor sera 
+    
+    indice_r2 = ALFABETO.index(letra_r1)
+    letra_r2 = wire_2[indice_r2]
+    
+    indice_r3 = ALFABETO.index(letra_r2)
+    salida = wire_3[indice_r3]
+    
+    return salida
+
+
